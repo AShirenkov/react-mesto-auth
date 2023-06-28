9dc09012ee22599af7592806672f9497b080d0f4
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-06-28 15:53:00 +0300

    feat: реализовать навигацию

--- src/components/App.js
+++ src/components/App.js
@@ -214,7 +214,7 @@ function App() {
   return (
     <CurrentUserContext.Provider value={currentUser}>
       <div className='page'>
-        <Header onLogout={handleLogout} />
+        <Header onLogout={handleLogout} isLoggedIn={isLoggedIn} currentEmail={currentEmail} />
         <Routes>
           <Route
             path='/'
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,42 @@
 import logo from '../images/logo.svg';
 import React from 'react';
-function Header({ onLogout }) {
+import { Link, useLocation } from 'react-router-dom';
+function Header({ onLogout, isLoggedIn, currentEmail }) {
+  const location = useLocation();
+  //console.log(onLogout);
   return (
     <header className='header'>
       <img src={logo} alt='Лого Место Россия' className='header__logo' />
+      <ul className='header__navi'>
+        {isLoggedIn && (
+          <li>
+            <p className='header__list-item header__email'></p>
+            {currentEmail}
+          </li>
+        )}
+
+        {isLoggedIn && (
+          <li>
+            <button className='header__list-item opacity-button' type='button' onClick={onLogout}>
+              Выход
+            </button>
+          </li>
+        )}
+        {!isLoggedIn && location.pathname === '/sing-up' && (
+          <li>
+            <Link className='header__list-item opacity-button' to='/sing-in'>
+              Войти
+            </Link>
+          </li>
+        )}
+        {!isLoggedIn && location.pathname === '/sing-in' && (
+          <li>
+            <Link className='header__list-item opacity-button' to='/sing-up'>
+              Регистрация
+            </Link>
+          </li>
+        )}
+      </ul>
     </header>
   );
 }
