3696628bd17561d05308acebc0f7619099deb8be
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-06-29 09:08:42 +0300

    fix: убрать комменты для улучшения читаемости кода

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -10,47 +10,27 @@ class Api {
     }
     return Promise.reject(`Ошибка сервера: ${res.status}`);
   }
-  //Нужно оптимизировать позже . убрать описание вызова _checkResponse в каждой точке
+
   _request(url, options) {
     return fetch(`${this._baseUrl}${url}`, options).then(this._checkResponse);
   }
 
   getInitialCards() {
-    //return fetch(`${this._baseUrl}/cards`, { headers: this._headers }).then(this._checkResponse);
     return this._request('/cards', { headers: this._headers });
   }
 
   getMyUser() {
-    //return fetch(`${this._baseUrl}/users/me`, { headers: this._headers }).then(this._checkResponse);
     return this._request('/users/me', { headers: this._headers });
   }
 
   setUserInfo(obj) {
-    // return fetch(`${this._baseUrl}/users/me`, {
-    //   method: 'PATCH',
-    //   headers: this._headers,
-    //   body: JSON.stringify(obj)
-    // }).then(this._checkResponse);
-
     return this._request('/users/me', {
       method: 'PATCH',
       headers: this._headers,
       body: JSON.stringify(obj)
     });
-
-    // return this._request('users/me', {
-    //   method: 'PATCH',
-    //   headers: this._headers,
-    //   body: JSON.stringify(obj)
-    // });
   }
   setUserAvatar(obj) {
-    // return fetch(`${this._baseUrl}/users/me/avatar`, {
-    //   method: 'PATCH',
-    //   headers: this._headers,
-    //   body: JSON.stringify(obj)
-    // }).then(this._checkResponse);
-
     return this._request('/users/me/avatar', {
       method: 'PATCH',
       headers: this._headers,
@@ -59,13 +39,6 @@ class Api {
   }
 
   sendNewCard(obj) {
-    //console.log(JSON.stringify(obj));
-    // return fetch(`${this._baseUrl}/cards`, {
-    //   method: 'POST',
-    //   headers: this._headers,
-    //   body: JSON.stringify(obj)
-    // }).then(this._checkResponse);
-
     return this._request('/cards', {
       method: 'POST',
       headers: this._headers,
@@ -74,10 +47,6 @@ class Api {
   }
 
   removeCard(cardId) {
-    // return fetch(`${this._baseUrl}/cards/${cardId}`, {
-    //   method: 'DELETE',
-    //   headers: this._headers
-    // }).then(this._checkResponse);
     return this._request(`/cards/${cardId}`, {
       method: 'DELETE',
       headers: this._headers
@@ -85,20 +54,12 @@ class Api {
   }
 
   removeLike(cardId) {
-    // return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-    //   method: 'DELETE',
-    //   headers: this._headers
-    // }).then(this._checkResponse);
     return this._request(`/cards/${cardId}/likes`, {
       method: 'DELETE',
       headers: this._headers
     });
   }
   setLike(cardId) {
-    // return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-    //   method: 'PUT',
-    //   headers: this._headers
-    // }).then(this._checkResponse);
     return this._request(`/cards/${cardId}/likes`, {
       method: 'PUT',
       headers: this._headers
