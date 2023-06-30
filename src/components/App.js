24ec9d7c4e9fdc7b906b463c8e3c43541ab8e9d6
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-06-30 10:41:54 +0300

    fix: Убрать лишний пропс у header

--- src/components/App.js
+++ src/components/App.js
@@ -222,7 +222,7 @@ function App() {
   return (
     <CurrentUserContext.Provider value={currentUser}>
       <div className='page'>
-        <Header onLogout={handleLogout} isLoggedIn={isLoggedIn} currentEmail={currentEmail} />
+        <Header onLogout={handleLogout} currentEmail={currentEmail} />
 
         <Routes>
           <Route
